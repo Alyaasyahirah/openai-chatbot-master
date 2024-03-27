52fce25be4a966131a76b6ff9042ec13985e0de6
Alyaasyahirah <138898120+Alyaasyahirah@users.noreply.github.com>
2024-03-27 08:00:50 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,8 @@ if prompt := st.chat_input("What is up?"):
             model="gpt-3.5-turbo",
          #   messages = [{"role": m["role"], "content": m["content"]}
             messages=[{"role": m["role"], "content": m["content"]}]
-                      for m in st.session_state.messages], stream=True):
+                     # for m in st.session_state.messages], stream=True):
+                      for m in [st.session_state.messages], stream=True):
             full_response += response.choices[0].delta.get("content", "")
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
